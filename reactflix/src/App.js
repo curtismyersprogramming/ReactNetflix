a278f1282ac63631b3d24e7c455ef0a2e5e46e1d
Curtis <curtismyersprogramming@gmail.com>
2022-07-18 00:18:06 +0100

    App Update

--- reactflix/src/App.js
+++ reactflix/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>ReactFlix</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Action" />
+      <Row title="Comedy" />
     </div>
   );
 }
